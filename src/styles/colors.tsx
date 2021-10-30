abdc849fc3eb5f2b8ad37cffe7b6670c887461b4
Hugo Ferreira <hugoferreirait@gmail.com>
2021-10-30 18:52:03 -0300

    chore: adding default colors

---
+++ src/styles/colors.tsx
@@ -0,0 +1,13 @@
+export default {
+    transparent: 'transparent',
+
+    primary: '#26D76D',
+    warning: '#f3bA2f',
+    danger: '#E95352',
+
+    white: '#ffffff',
+    gray: '#5C645F',
+    lightGray: '#BCBCBC',
+    mediumGray: '#3D3F3D',
+    darkerGray: '#252525',
+}

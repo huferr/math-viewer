104c595f7ebd5e908c1ac2087105be6d55595efc
Hugo Ferreira <hugoferreirait@gmail.com>
2021-10-30 23:03:00 -0300

    fixing: react native update forced

--- android/app/src/main/java/com/mathviewer/MainActivity.java
+++ android/app/src/main/java/com/mathviewer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mathvisualizer;
+package com.mathviewer;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mathvisualizer";
+    return "mathviewer";
   }
 }
--- ios/mathviewerTests/mathviewerTests.m
+++ ios/mathviewerTests/mathviewerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mathvisualizerTests : XCTestCase
+@interface mathviewerTests : XCTestCase
 
 @end
 
-@implementation mathvisualizerTests
+@implementation mathviewerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

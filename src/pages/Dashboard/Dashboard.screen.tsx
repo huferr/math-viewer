cadd5976b6621b4867f9d85b36a628337a00f1f8
Hugo Ferreira <hugoferreirait@gmail.com>
2021-10-31 23:24:34 -0300

    chore: installing and configuring eslint

--- src/pages/Dashboard/Dashboard.screen.tsx
+++ src/pages/Dashboard/Dashboard.screen.tsx
@@ -3,8 +3,8 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Dashboard: React.FC = () => {
-    return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'red',}}>
-        <Text>testing</Text>
-    </SafeAreaView>)
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: "red",}}>
+      <Text>testing</Text>
+    </SafeAreaView>);
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import AppRoutes from "./app.routes";
 
 export const Routes: React.FC = () => {
-
-    return <AppRoutes />
-}
+  return <AppRoutes />;
+};

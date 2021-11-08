3c2ae836b62375f619a203210e60e6bddf1700c5
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-07 22:28:14 -0300

    fix: changing LF to CRLF eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
-import { Routes } from "./routes"
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Routes } from "./routes";
 
 export const App: React.FC = () => {
-    return (
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+};

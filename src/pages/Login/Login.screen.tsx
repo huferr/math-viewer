3f4b412451fc421096548b4cb9d49b30d131782d
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-15 18:13:48 -0300

    feat: login screen design

--- src/pages/Login/Login.screen.tsx
+++ src/pages/Login/Login.screen.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Container } from "./Login.styles";
+import { FullPage, Input } from "../../components";
+import { AuthorText, FooterMessage, PhraseText, Pi } from "./Login.styles";
 
 export const Login: React.FC = () => {
   return (
-    <Container>
-        
-    </Container>
+    <FullPage 
+      whiteTitle="Login to" 
+      greenTitle="Math" 
+      goBack
+      buttons
+      buttonPrimary
+      buttonSecondary
+      footerMessage={
+        <FooterMessage>
+          <PhraseText>&quot;The book of the world is written in mathematical language.&quot;</PhraseText>
+          <AuthorText>- Galileu Galilei</AuthorText>
+        </FooterMessage>
+      }
+    >
+      <Pi>π</Pi>
+      <Input style={{marginTop: 70}} placeholder="Email" errorText="Email do caralho"/>
+      <Input style={{marginTop: 20}} placeholder="Password" errorText="Senha do caralho"/>
+    </FullPage>
   );
 };
--- src/pages/Login/Login.styles.tsx
+++ src/pages/Login/Login.styles.tsx
@@ -1,7 +1,29 @@
 import styled from "styled-components/native";
 import colors from "../../styles/colors";
 
-export const Container  = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${colors.darkerGray};
+export const Pi  = styled.Text`
+  margin-top: 10px;
+  font-size: 64px;
+  font-weight: 300;
+  text-align: center;
+  color: ${colors.primary};
+`;
+
+export const FooterMessage = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PhraseText = styled.Text`
+  color: ${colors.white};
+  font-style: italic;
+  font-weight: 700;
+  font-size: 11.2px;
+`;
+export const AuthorText = styled.Text`
+  margin-top: 10px;
+  color: ${colors.white};
+  font-style: italic;
+  font-size: 12px;
+  align-self: flex-end;
 `;

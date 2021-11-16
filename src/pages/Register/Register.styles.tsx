856be26ecef92378f20e065e59a0e4e337c2df39
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-15 23:33:25 -0300

    feat: creating register screen design

---
+++ src/pages/Register/Register.styles.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+import colors from "../../styles/colors";
+
+export const Lambda = styled.Text`
+  margin-top: 10px;
+  font-size: 64px;
+  font-weight: 300;
+  color: ${colors.white};
+  text-align: center;
+`;
+
+export const FooterMessage = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PhraseText = styled.Text`
+  color: ${colors.white};
+  font-style: italic;
+  font-weight: 700;
+  font-size: 11.2px;
+`;
+export const AuthorText = styled.Text`
+  margin-top: 10px;
+  color: ${colors.white};
+  font-style: italic;
+  font-size: 12px;
+  align-self: flex-end;
+`;

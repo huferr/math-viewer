2708e0e1a1fb9a39c72c4da519af83fe83fc9e89
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-15 23:31:27 -0300

    chore: refactor welcome screen

--- src/pages/Welcome/Welcome.screen.tsx
+++ src/pages/Welcome/Welcome.screen.tsx
@@ -1,26 +1,39 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/core";
 import { Linking, TouchableOpacity } from "react-native";
 import { GraphIcon } from "../../assets/icons/graph";
-import { Button } from "../../components/Button/Button.component";
-import { Body, Container, IconWrapper, Lambda, Pi, Title, WhiteTitle, ByText, NameText } from "./Welcome.styles";
+import { FullPage } from "../../components";
+import { NavigateTo } from "../../helpers/navigateTo";
+import { IconWrapper, Lambda, Pi, Title, WhiteTitle, ByText, NameText } from "./Welcome.styles";
 
 export const Welcome: React.FC = () => {
+
+  const navigation = useNavigation();
+
+  const handleFooterMessage = () => (
+    <TouchableOpacity activeOpacity={0.8} onPress={() => Linking.openURL("https://github.com/huferr")}>
+      <ByText>by{" "}<NameText>huferr</NameText>
+      </ByText>
+    </TouchableOpacity>
+  );
+
   return (
-    <Container>
-      <Body>
-        <Lambda>λ</Lambda>
-        <Title>Math{"\n"}<WhiteTitle>Viewer</WhiteTitle></Title>
-        <IconWrapper>
-          <Pi>π</Pi>
-          <GraphIcon/>
-        </IconWrapper>
-        <Button type="secondary" title="Sign Up" style={{marginBottom: 30}}/>
-        <Button type="primary" title="Login" style={{marginBottom: 60}}/>
-        <TouchableOpacity activeOpacity={0.8} onPress={() => Linking.openURL("https://github.com/huferr")}>
-          <ByText>by{" "}<NameText>huferr</NameText>
-          </ByText>
-        </TouchableOpacity>
-      </Body>
-    </Container>
+    <FullPage
+      buttons
+      buttonPrimary
+      buttonPrimaryTitle="Login"
+      onPressPrimary={() => NavigateTo("login", navigation, {})}
+      buttonSecondary
+      buttonSecondaryTitle="Sign Up"
+      onPressSecondary={() => NavigateTo("signup", navigation, {})}
+      footerMessage={handleFooterMessage()}
+    >
+      <Lambda>λ</Lambda>
+      <Title>Math{"\n"}<WhiteTitle>Viewer</WhiteTitle></Title>
+      <IconWrapper>
+        <Pi>π</Pi>
+        <GraphIcon/>
+      </IconWrapper>
+    </FullPage>
   );
 };

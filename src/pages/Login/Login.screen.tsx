79082e1828c84b38952b599d70b1e21ee526da0c
Hugo Ferreira <hugoferreirait@gmail.com>
2021-11-15 23:32:25 -0300

    fix: refactor login screen

--- src/pages/Login/Login.screen.tsx
+++ src/pages/Login/Login.screen.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { FullPage, Input } from "../../components";
 import { AuthorText, FooterMessage, PhraseText, Pi } from "./Login.styles";
+import { useNavigation } from "@react-navigation/core";
 
 export const Login: React.FC = () => {
+
+  const navigation = useNavigation();
+  const goBack = () => navigation.goBack();
+
   return (
     <FullPage 
       whiteTitle="Login to" 
       greenTitle="Math" 
       goBack
+      onPressGoBack={() => goBack()}
       buttons
       buttonPrimary
+      buttonPrimaryTitle="Continue"
       buttonSecondary
+      buttonSecondaryTitle="Forgot my password"
       footerMessage={
         <FooterMessage>
           <PhraseText>&quot;The book of the world is written in mathematical language.&quot;</PhraseText>
@@ -20,7 +28,7 @@ export const Login: React.FC = () => {
     >
       <Pi>π</Pi>
       <Input style={{marginTop: 70}} placeholder="Email" errorText="Email do caralho"/>
-      <Input style={{marginTop: 20}} placeholder="Password" errorText="Senha do caralho"/>
+      <Input style={{marginTop: 10}} placeholder="Password" errorText="Senha do caralho"/>
     </FullPage>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,3 @@
 export { Welcome } from "./Welcome";
+export { Login } from "./Login";
+export { Register } from "./Register";

5105763f20e1e958de9798e95094a873853d24f5
Hugo Ferreira <hugoferreirait@gmail.com>
2021-12-24 02:58:10 -0300

    fix: adjusting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,11 @@ module.exports = {
     "semi": [
       "error",
       "always"
-    ]
+    ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["off"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
   }
 };

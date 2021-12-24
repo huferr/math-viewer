a67bcf61044b50fb7dc5fbb717402e0f2df35cd9
Hugo Ferreira <hugoferreirait@gmail.com>
2021-12-24 03:00:35 -0300

    fix: adjusting layout

---
+++ src/pages/RegisterFlow/1-Register.screen.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FullPage, Input } from "../../components";
+import { AuthorText, FooterMessage, TopIcon, PhraseText } from "./Register.styles";
+import { useNavigation } from "@react-navigation/core";
+import { NavigateTo } from "../../helpers/navigateTo";
+
+export const Register: React.FC = () => {
+
+  const navigation = useNavigation();
+  const goBack = () => navigation.goBack();
+
+  return (
+    <FullPage 
+      whiteTitle="Register to" 
+      greenTitle="Math" 
+      goBack
+      onPressGoBack={() => goBack()}
+      onPressPrimary={() => NavigateTo("verifyEmail", navigation, {})}
+      buttons
+      onlyOneButton
+      buttonPrimary
+      buttonPrimaryTitle="Continue"
+      verticalBounce={false}
+    >
+      <TopIcon>λ</TopIcon>
+      <Input style={{marginTop: 10}} placeholder="Nickname" errorText="Nickname do caralho"/>
+      <Input style={{marginTop: 10}} placeholder="Email" errorText="Email do caralho"/>
+      <Input style={{marginTop: 10}} placeholder="Password" errorText="Password do caralho"/>
+      <Input style={{marginTop: 10}} placeholder="Confirm" errorText="Confirm password do caralho"/>
+    </FullPage>
+  );
+};

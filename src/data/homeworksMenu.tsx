5c8c62fdb7a581198ca41efe428167cc3ab615d9
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-04 22:37:18 -0300

    chore: creating homeworks menu data

---
+++ src/data/homeworksMenu.tsx
@@ -0,0 +1,27 @@
+interface HomeworksMenu {
+  id: string,
+  title: string,
+  subtitle: string,
+  completed: boolean,
+}
+
+export const homeworksMenu: HomeworksMenu[] = [
+  {
+    id: "geometry",
+    title: "Geometry",
+    subtitle: "Calculate area, volume and perimeter of geometric shapes.",
+    completed: false,
+  },
+  {
+    id: "analytic_geometry",
+    title: "Analytic geometry",
+    subtitle: "Calculate area, volume and perimeter of geometric shapes.",
+    completed: false,
+  },
+  {
+    id: "geometry",
+    title: "Geometry",
+    subtitle: "Calculate area, volume and perimeter of geometric shapes.",
+    completed: false,
+  },
+];

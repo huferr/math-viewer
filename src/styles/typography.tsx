91ebdb8f0571d2fdb0b0ea0875e0461521e1fcab
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-08 02:26:03 -0300

    chore: creating typography

--- src/styles/typography.tsx
+++ src/styles/typography.tsx
@@ -1,8 +1,26 @@
 import styled from "styled-components/native";
+import colors from "./colors";
 
-// base headings, subtitles and paragraphs
-export const Heading = styled.Text``;
+interface TypographyProps {
+  bold?: boolean;
+  green?: boolean;
+  marginTop?: number;
+  marginBottom?: number;
+}
 
-export const Subtitle = styled.Text``;
+export const Heading = styled.Text<TypographyProps>`
+  font-size: 36px;
+  font-weight: ${props => props.bold ? 700 : 300};
+  color: ${props => props.green ? colors.primary : colors.white};
+`;
+
+export const Subtitle = styled.Text<TypographyProps>`
+  font-size: 18px;
+  margin-top: ${props => props.marginTop || 0}px;
+  margin-bottom: ${props => props.marginBottom || 0}px;
+  color: ${props => props.green ? colors.primary : colors.white};
+  font-weight: ${props => props.bold ? 700 : 300};
+  text-align: center;
+`;
 
 export const Paragraph = styled.Text``;

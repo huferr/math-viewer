5e668452429b1cfbac7b6c67b75834b0662f34f9
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-09 02:47:32 -0300

    feat: profile and search svg icons

---
+++ src/assets/icons/profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Path, Svg } from "react-native-svg";
+
+export function ProfileIcon(props: React.SVGProps<Svg>): JSX.Element {
+  const { width = 20, height = 20, fill = "none" } = props;
+  
+  return (
+    <Svg width={width} height={height} viewBox="0 0 20 20" fill={fill}>
+      <Path 
+        d="M14.5556 5.51852C14.5556 8.01403 12.5326 10.037 10.0371 10.037C7.54156 10.037 5.51855 8.01403 5.51855 5.51852C5.51855 3.02301 7.54156 1 10.0371 1C12.5326 1 14.5556 3.02301 14.5556 5.51852Z"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />
+      <Path 
+        d="M1 18.4286L1.21484 18.0756C5.23873 11.4649 14.8353 11.4649 18.8592 18.0756L19.0741 18.4286" 
+        stroke="white"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"/>
+    </Svg>
+  );
+}
---
+++ src/assets/icons/search.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Path, Svg } from "react-native-svg";
+
+export function SearchIcon(props: React.SVGProps<Svg>): JSX.Element {
+  const { width = 18, height = 18, fill = "none" } = props;
+
+  return (
+    <Svg 
+      width={width}
+      height={height}
+      viewBox="0 0 18 18"
+      fill={fill}
+    >
+      <Path 
+        d="M9.67506 3.28767C11.387 5.10341 11.3028 7.96314 9.48709 9.67506C7.67136 11.387 4.81163 11.3028 3.09971 9.48709C1.38779 7.67136 1.47194 4.81163 3.28767 3.09971C5.10341 1.38779 7.96314 1.47194 9.67506 3.28767Z"
+        stroke={"white"}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"/>
+      <Path
+        d="M11.1749 11.4687L16.6483 17.3032"
+        stroke="white"
+        strokeWidth="1.5"/>
+    </Svg>
+  );
+}
+  
+

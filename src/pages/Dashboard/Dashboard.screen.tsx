5738a1810d77a81c44bc2380f83ab592fd2e35e5
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-09 21:31:19 -0300

    feat: dashboard screen layout part 1

--- src/pages/Dashboard/Dashboard.screen.tsx
+++ src/pages/Dashboard/Dashboard.screen.tsx
@@ -1,10 +1,57 @@
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { GraphIcon, ProfileIcon, SearchIcon } from "../../assets/icons";
+import { BigCard, FullPage } from "../../components";
+import { HeadingSmall } from "../../styles";
+import { Button, Card, CardWrapper, Header } from "./Dashboard.styles";
 
 export const Dashboard: React.FC = () => {
+
+  const HelloUser = (
+    <HeadingSmall bold>
+      Hello,
+      {" "}
+      <HeadingSmall green>Hugo</HeadingSmall>
+    </HeadingSmall>
+  );
+
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: "red",}}>
-      <Text>testing</Text>
-    </SafeAreaView>);
+    <FullPage>
+
+      <Header>
+        <Button>
+          <SearchIcon />
+        </Button>
+        {HelloUser}
+        <Button>
+          <ProfileIcon />
+        </Button>
+      </Header>
+
+      <CardWrapper>
+        <Card>
+          <HeadingSmall green italic>Homeworks</HeadingSmall>
+          <HeadingSmall italic>99/99</HeadingSmall>
+        </Card>
+        <Card>
+          <HeadingSmall green italic>Mathscore</HeadingSmall>
+          <HeadingSmall italic>9999</HeadingSmall>
+        </Card>
+      </CardWrapper>
+
+      <BigCard 
+        title="Viewer"
+        subtitle="Explore functions and numbers to see what they show visually!"
+        icon={<GraphIcon width={130} height={100} />}
+        onPress={() => {}}
+        marginBottom={20}  
+      />
+
+      <BigCard 
+        title="Homeworks"
+        subtitle="Explore functions and numbers to see what they show visually!"
+        icon={<GraphIcon width={130} height={100} />}
+        onPress={() => {}}  
+      />
+    </FullPage>
+  );
 };
---
+++ src/pages/Dashboard/Dashboard.styles.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+
+export const Button = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const CardWrapper = styled.View`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Card = styled.TouchableOpacity`
+  align-items: center;
+  width: 150px;
+`;

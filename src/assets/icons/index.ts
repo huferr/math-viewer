31d667e4e64eaa860e22447ca961c72ce0e324fb
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-15 20:05:33 -0300

    feat: edit icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { BigCircle } from "./big-circle";
+export { EditIcon } from "./edit";
 export { GoBackIcon } from "./go-back";
 export { GraphIcon } from "./graph";
 export { ProfileIcon } from "./profile";

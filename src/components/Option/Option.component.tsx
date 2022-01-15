763ff412797c7810f7e1816e48d3494cb59731b9
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-15 20:15:06 -0300

    feat: Option component

--- src/components/Option/Option.component.tsx
+++ src/components/Option/Option.component.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Container } from "./Option.styles";
+import { EditIcon } from "../../assets/icons";
+import { Paragraph } from "../../styles";
+import { Container, ContentWrapper, IconWrapper } from "./Option.styles";
 
-export const Option: React.FC = () => {
+interface OptionProps {
+  title: string;
+  content: string;
+  onPress: () => void;
+}
 
+export const Option: React.FC<OptionProps> = (props) => {
+  const { title, content, onPress} = props;
   return (
-    <Container>
-        
+    <Container onPress={onPress}>
+      <ContentWrapper>
+        <Paragraph green marginTop={5} marginBottom={5}>{title}</Paragraph>
+        <Paragraph>{content}</Paragraph>
+      </ContentWrapper>
+      <IconWrapper>
+        <EditIcon />
+      </IconWrapper>
     </Container>
   );
 };
--- src/components/Option/Option.styles.tsx
+++ src/components/Option/Option.styles.tsx
@@ -3,8 +3,17 @@ import { colors } from "../../styles";
 
 export const Container = styled.TouchableOpacity.attrs({ activeOpacity: 0.7 })`
   width: 100%;
-  height: 80px;
+  height: 50px;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-bottom: 40px;
   border-top-style: solid;
   border-top-color: ${colors.gray};
   border-top-width: 2px;
+`;
+
+export const ContentWrapper = styled.View``;
+
+export const IconWrapper = styled.View`
+  justify-content: flex-end;
 `;

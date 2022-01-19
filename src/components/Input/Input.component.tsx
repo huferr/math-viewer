1df3004da0f73d097d979984825b6238c2e9cedb
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-19 01:03:16 -0300

    chore: adding "minimal" input type

--- src/components/Input/Input.component.tsx
+++ src/components/Input/Input.component.tsx
@@ -4,18 +4,20 @@ import { colors } from "../../styles";
 import { Container, ErrorContainer, ErrorText, InputContainer, InputField } from "./input.styles";
 
 export interface InputProps {
+  type: "primary" | "minimal",
   style?: StyleProp<ViewStyle>,
   placeholder?: string,
   errorText?: string,
 }
 
 export const Input: React.FC<InputProps> = (props) => {
-  const { style, placeholder, errorText } = props;
+  const { style, placeholder, errorText, type } = props;
 
   return (
     <Container style={style}>
-      <InputContainer>
+      <InputContainer type={type}>
         <InputField 
+          type={type}
           placeholder={placeholder} 
           placeholderTextColor={colors.lightGray} 
           selectionColor={colors.primary}
--- src/components/Input/input.styles.tsx
+++ src/components/Input/input.styles.tsx
@@ -1,22 +1,35 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { colors } from "../../styles";
+import { InputProps } from "./Input.component";
+
+const backgroundColors: Record<InputProps["type"], string> = {
+  primary: colors.gray,
+  minimal: colors.transparent,
+};
+
+const mininalInput = css`
+  border-bottom-style: solid;
+  border-bottom-color: ${colors.gray};
+  border-bottom-width: 2px;
+`;
 
 export const Container = styled.View`
   width: 100%;
 `;
 
-export const InputContainer = styled.View`
+export const InputContainer = styled.View<InputProps>`
   width: 100%;
-  background-color: ${colors.gray};
-  border-radius: 12px;
+  background-color: ${(props) => backgroundColors[props.type]};
+  border-radius: ${(props) => props.type === "minimal" ? 0 : 12}px;
 `;
 
-export const InputField = styled.TextInput`
+export const InputField = styled.TextInput<InputProps>`
   width: 100%;
-  height: 50px;
-  padding-left: 15px;
+  height: ${(props) => props.type === "minimal" ? 35 : 50}px;
+  padding-left: ${(props) => props.type === "minimal" ? 5 : 15}px;
   font-size: 16px;
   color: ${colors.white};
+  ${(props) => props.type === "minimal" && mininalInput};
 `;
 
 export const ErrorContainer = styled.View`

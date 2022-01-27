7bcbd1e6927d3b587687b2bb4e6d774c3d2d330c
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-27 00:28:28 -0300

    chore; refactor search icon

--- src/assets/icons/search.tsx
+++ src/assets/icons/search.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Path, Svg } from "react-native-svg";
 
 export function SearchIcon(props: React.SVGProps<Svg>): JSX.Element {
-  const { width = 25, height = 25, fill = "none" } = props;
+  const { width = 25, height = 25, fill = "none", stroke = "white" } = props;
 
   return (
     <Svg 
@@ -13,13 +13,13 @@ export function SearchIcon(props: React.SVGProps<Svg>): JSX.Element {
     >
       <Path 
         d="M9.67506 3.28767C11.387 5.10341 11.3028 7.96314 9.48709 9.67506C7.67136 11.387 4.81163 11.3028 3.09971 9.48709C1.38779 7.67136 1.47194 4.81163 3.28767 3.09971C5.10341 1.38779 7.96314 1.47194 9.67506 3.28767Z"
-        stroke={"white"}
+        stroke={stroke}
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"/>
       <Path
         d="M11.1749 11.4687L16.6483 17.3032"
-        stroke="white"
+        stroke={stroke}
         strokeMiterlimit="10"
         strokeWidth="1.5"/>
     </Svg>

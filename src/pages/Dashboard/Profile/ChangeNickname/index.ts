8cc0c9aa0a90ecdb6dd697b78e23d95264a5a380
Hugo Ferreira <hugoferreirait@gmail.com>
2022-01-26 21:36:41 -0300

    chore: change nickname fodler structure

--- src/pages/Dashboard/Profile/ChangeNickname/index.ts
+++ src/pages/Dashboard/Profile/ChangeNickname/index.ts
@@ -1,2 +1,2 @@
-export { ChangeNickname } from "./ChangeNickname.screen";
+export { ChangeNickname } from "./1.ChangeNickname.screen";
 export { SuccessChangeNickname } from "./2.SuccessChangeNickname.screen";

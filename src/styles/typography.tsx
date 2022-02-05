6c6fa79c6d47314ef938d18c21ef87eb21c34ae6
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-05 00:26:37 -0300

    chore: adding margin props to typography

--- src/styles/typography.tsx
+++ src/styles/typography.tsx
@@ -16,6 +16,8 @@ export const Heading = styled.Text<TypographyProps>`
   text-align: ${props => props.textAlign || "left"};
   font-weight: ${props => props.bold ? 700 : 300};
   color: ${props => props.green ? colors.primary : colors.white};
+  margin-top: ${props => props.marginTop || 0}px;
+  margin-bottom: ${props => props.marginBottom || 0}px;
 `;
 
 export const HeadingSmall = styled.Text<TypographyProps>`
@@ -24,24 +26,26 @@ export const HeadingSmall = styled.Text<TypographyProps>`
   font-style: ${props => props.italic ? "italic" : "normal"};
   text-align: ${props => props.textAlign || "left"};
   color: ${props => props.green ? colors.primary : colors.white};
+  margin-top: ${props => props.marginTop || 0}px;
+  margin-bottom: ${props => props.marginBottom || 0}px;
 `;
 
 export const Subtitle = styled.Text<TypographyProps>`
   font-size: 18px;
   font-style: ${props => props.italic ? "italic" : "normal"};
-  margin-top: ${props => props.marginTop || 0}px;
-  margin-bottom: ${props => props.marginBottom || 0}px;
   color: ${props => props.green ? colors.primary : colors.white};
   font-weight: ${props => props.bold ? 700 : 300};
   text-align: ${props => props.textAlign || "left"};
+  margin-top: ${props => props.marginTop || 0}px;
+  margin-bottom: ${props => props.marginBottom || 0}px;
 `;
 
 export const Paragraph = styled.Text<TypographyProps>`
   font-size: 16px;
-  margin-top: ${props => props.marginTop || 0}px;
-  margin-bottom: ${props => props.marginBottom || 0}px;
   font-weight: ${props => props.bold ? 700 : 300};
   font-style: ${props => props.italic ? "italic" : "normal"};
   color: ${props => props.green ? colors.primary : colors.white};
   text-align: ${props => props.textAlign || "left"};
-`;
+  margin-top: ${props => props.marginTop || 0}px;
+  margin-bottom: ${props => props.marginBottom || 0}px;
+  `;

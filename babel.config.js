22238667c3501992d82d97f56fde66c23680d158
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-13 20:58:32 -0300

    chore: simplifying import ~/ alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        alias: {
+          /**
+           * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
+           */
+          "^~(.+)": "./src/\\1",
+        },
+        extensions: [
+          ".ios.js",
+          ".android.js",
+          ".js",
+          ".jsx",
+          ".json",
+          ".tsx",
+          ".ts",
+          ".native.js",
+        ],
+      },
+    ],
+  ],
 };

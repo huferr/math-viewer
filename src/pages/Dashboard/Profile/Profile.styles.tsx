dfda5457da8eb8330701e90df96eaac638c57b1f
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-15 09:26:54 -0300

    feat: add image picker

--- src/pages/Dashboard/Profile/Profile.styles.tsx
+++ src/pages/Dashboard/Profile/Profile.styles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { colors } from "~styles";
 
 export const ProfilePicContainer = styled.View`
   width: 100%;
@@ -6,6 +7,13 @@ export const ProfilePicContainer = styled.View`
   margin-bottom: 100px;
   justify-content: center;
   align-items: center;
+`;
+
+export const UserImage = styled.Image`
+  width: 120px;
+  height: 120px;
+  border-radius: 100px;
+  background-color: ${colors.primary};
 `;
 
 export const Button = styled.TouchableOpacity.attrs({ activeOpacity: 0.7 })``;

73fa01bddd939df78ccf1c30222071963356bd29
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-26 17:22:39 -0300

    feat: adding redux

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { viewerInfoModalReducer } from "./slices/InfoModal.slice";
+
+export const store = configureStore({
+  reducer: {
+    infoModal: viewerInfoModalReducer,
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch

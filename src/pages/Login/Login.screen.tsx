28f1c3ff0494262d3fa6ad446f9fbf881bc4a3fc
Hugo Ferreira <hugoferreirait@gmail.com>
2022-02-26 22:36:30 -0300

    feat: create verifyEmailFor global state

--- src/pages/Login/Login.screen.tsx
+++ src/pages/Login/Login.screen.tsx
@@ -3,13 +3,19 @@ import { FullPage, Input } from "~/components";
 import { Pi } from "./Login.styles";
 import { useNavigation } from "@react-navigation/core";
 import { NavigateTo } from "~/services";
+import { useAppDispatch } from "~app/hooks";
+import { verifyEmailFor } from "~app/slices/verifyEmailFor.slice";
 
 export const Login: React.FC = () => {
 
+  const dispatch = useAppDispatch();
   const navigation = useNavigation();
   
   const goBack = () => NavigateTo("welcome", navigation, {});
-  const goToVerifyEmail = () => NavigateTo("verify_email", navigation, { isToDashboard: true });
+  const goToVerifyEmail = () => {
+    NavigateTo("verify_email", navigation, { isToDashboard: true });
+    dispatch(verifyEmailFor("login"));
+  };
   const goToRecoveryPassword = () => NavigateTo("enter_recovery_email", navigation, {});
 
   return (

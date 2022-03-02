e5dbaba38b057aec0a66bda7b56538491c4f9f83
Hugo Ferreira <hugoferreirait@gmail.com>
2022-03-01 21:30:18 -0300

    feat: login implementation 1/2

---
+++ src/services/general/storage.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeData = async (key: string, value: string) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getData = async (key: string) => {
+  try {
+    const data = await AsyncStorage.getItem(key);
+
+    if (data !== null) {
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/typings/mutations/useLogin.ts
@@ -0,0 +1,15 @@
+export declare namespace UseLoginTypes {
+    interface Params {
+        userLoginInput: {
+          email: string,
+          password: string,
+        }
+    }
+
+    interface Response {
+        login: {
+          accessToken: string,
+        }
+    }
+    
+}
